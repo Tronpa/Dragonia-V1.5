f4a3ed0465d85c5908b491920e47ca43c1b5f3d9
Tronpa <33377363+Tronpa@users.noreply.github.com>
2019-09-28 00:42:05 +0200

    Add files via upload

---
+++ uploads/index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTPS_HOST'];
+	header('Location: '.$uri.'tronpa.fr/pages/');
+	exit;
+?>
